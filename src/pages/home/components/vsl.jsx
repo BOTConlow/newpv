355649214c054cbf2c19becec7ac9c7a0a3b2c26
Victor <uskenmellorios@gmail.com>
2023-07-13 21:35:22 -0300

    commit

--- src/pages/home/components/vsl.jsx
+++ src/pages/home/components/vsl.jsx
@@ -8,10 +8,9 @@ export function Vsl() {
 
             <iframe
                 id="panda-20943242-edc0-41cb-a0f3-177d882537a8"
-                src="https://player-vz-cb3e51cc-16c.tv.pandavideo.com.br/embed/?v=20943242-edc0-41cb-a0f3-177d882537a8&playOpensFullscreenNative=true"
-                style={{ border: 'none' }}
+                src="https://player-vz-cb3e51cc-16c.tv.pandavideo.com.br/embed/?v=20943242-edc0-41cb-a0f3-177d882537a8"
                 allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
-                allowfullscreen="true"
+                style={{ border: 'none' }}
                 width="720"
                 height="360"
                 fetchpriority="high">
