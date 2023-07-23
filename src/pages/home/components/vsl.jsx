0b7fd189988ec14b664ec8199fc3a9e26e3802f0
Victor <uskenmellorios@gmail.com>
2023-07-23 04:10:29 -0300

    Pronto

--- src/pages/home/components/vsl.jsx
+++ src/pages/home/components/vsl.jsx
@@ -7,10 +7,10 @@ export function Vsl() {
         <div className={style.vsl}>
 
             <iframe
-                id="panda-20943242-edc0-41cb-a0f3-177d882537a8"
-                src="https://player-vz-cb3e51cc-16c.tv.pandavideo.com.br/embed/?v=20943242-edc0-41cb-a0f3-177d882537a8"
-                allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
+                id="panda-05963ef3-30c1-49f0-8f7b-1ddc780404a9"
+                src="https://player-vz-cb3e51cc-16c.tv.pandavideo.com.br/embed/?v=05963ef3-30c1-49f0-8f7b-1ddc780404a9"
                 style={{ border: 'none' }}
+                allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
                 width="720"
                 height="360"
                 fetchpriority="high">
