ce3a63f9b4788d3896830554d8b40efb9b635a98
Victor <uskenmellorios@gmail.com>
2023-07-24 21:15:25 -0300

    new vsl update

--- src/pages/home/components/vsl.jsx
+++ src/pages/home/components/vsl.jsx
@@ -13,8 +13,16 @@ export function Vsl() {
                 allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
                 width="720"
                 height="360"
-                fetchpriority="high">
-            </iframe>
+                fetchpriority="high"
+            />
+
+            <iframe
+                id="panda-05963ef3-30c1-49f0-8f7b-1ddc780404a9"
+                src="https://player-vz-cb3e51cc-16c.tv.pandavideo.com.br/embed/?v=05963ef3-30c1-49f0-8f7b-1ddc780404a9" style="border:none;" allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
+                allowFullScreen
+                width={720}
+                height={360}
+                fetchpriority="high" />
 
         </div>
 
